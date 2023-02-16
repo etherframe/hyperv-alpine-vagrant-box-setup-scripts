598a9935b20b0e566afed624de761ff017c00c0c
alecrodden <alecrodden@users.noreply.github.com>
2023-02-15 16:13:49 -0800

    give root permission to doas other users

--- alpine-box-setup.sh
+++ alpine-box-setup.sh
@@ -2,15 +2,18 @@
 
 apk add --no-cache --upgrade doas openssl hvtools cifs-utils
 
+DOAS_CONF="/etc/ssh/sshd_config"
+echo "permit nopass root" >> "$DOAS_CONF"
+
 adduser -D vagrant
-echo 'vagrant:vagrant' | chpasswd
-echo 'permit nopass vagrant as root' >> /etc/doas.d/doas.conf
+echo "vagrant:vagrant" | chpasswd
+echo "permit nopass vagrant as root" >> "$DOAS_CONF"
 
 doas -u vagrant mkdir -m0700 -p /home/vagrant/.ssh
 doas -u vagrant wget -O /home/vagrant/.ssh/authorized_keys https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub
 chmod 0600 /home/vagrant/.ssh/authorized_keys
 
-echo 'UseDNS no' >> /etc/ssh/sshd_config
+echo "UseDNS no" >> /etc/ssh/sshd_config
 
 rc-update add hv_fcopy_daemon
 rc-update add hv_kvp_daemon
